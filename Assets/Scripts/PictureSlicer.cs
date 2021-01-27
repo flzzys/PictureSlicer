84176b76b074d2dc36d849c086f4388474eb8c6c
Zyf <flzzyf@sina.com>
2021-01-27 10:58:34 +0800

    重制完成

--- Assets/Scripts/PictureSlicer.cs
+++ Assets/Scripts/PictureSlicer.cs
@@ -3,13 +3,202 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PictureSlicer : MonoBehaviour {
-    public SizeManipulator sizeManipulator;
+    public RectTransform picture;
 
+    //图片尺寸
     Vector2 size;
 
+    //把手
+    public PictureSlicerHandle handle_Main;
+    public PictureSlicerHandle handle_TopLeft;
+    public PictureSlicerHandle handle_TopRight;
+    public PictureSlicerHandle handle_BottomLeft;
+    public PictureSlicerHandle handle_BottomRight;
+
     private void Awake() {
-        size = GetComponent<RectTransform>().sizeDelta;
+        size = picture.sizeDelta;
 
-
+        InitMoving();
+        InitHandles();
     }
+
+    private void Update() {
+        UpdateMoving();
+        UpdateHandles();
+
+        if (Input.GetKeyDown("1")) {
+            Fill();
+        }
+        if (Input.GetKeyDown("2")) {
+            Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Set(picture.transform.position, mouse);
+        }
+    }
+
+    #region 截取器移动
+
+    //移动中
+    bool moving;
+
+    //鼠标和本体的偏移量
+    Vector2 mouseOffset;
+
+    //初始化移动数据
+    void InitMoving() {
+        handle_Main.onMouseDown = handle => {
+            moving = true;
+
+            mouseOffset = Camera.main.WorldToScreenPoint(handle_Main.transform.position) - Input.mousePosition;
+        };
+        handle_Main.onMouseUp = handle => {
+            moving = false;
+        };
+    }
+
+    //更新移动
+    void UpdateMoving() {
+        if (moving) {
+            //移动的目标位置
+            Vector2 desiredPos = (Vector2)Input.mousePosition + mouseOffset;
+
+            TryMove(desiredPos);
+        }
+    }
+
+    //尝试移动，不会移动到范围外
+    void TryMove(Vector2 uiPos) {
+        //将目标位置限制在可动范围内
+        uiPos = GetClampedPos(uiPos);
+
+        //移动
+        handle_Main.transform.position = (Vector2)Camera.main.ScreenToWorldPoint(uiPos);
+    }
+
+    //将目标位置限制在可动范围内
+    Vector2 GetClampedPos(Vector2 uiPos) {
+        //获取可动范围内
+        Vector2 center = Camera.main.WorldToScreenPoint(picture.transform.position);
+        Vector2 movementAreaTopRight = center + size / 2 - mainHandleSize / 2;
+        Vector2 movementAreaBottomLeft = center - size / 2 + mainHandleSize / 2;
+
+        //将目标位置限制在可动范围内
+        uiPos.x = Mathf.Clamp(uiPos.x, movementAreaBottomLeft.x, movementAreaTopRight.x);
+        uiPos.y = Mathf.Clamp(uiPos.y, movementAreaBottomLeft.y, movementAreaTopRight.y);
+
+        return uiPos;
+    }
+
+    #endregion
+
+    #region 缩放把手们
+
+    Dictionary<PictureSlicerHandle, Vector2> handleDic = new Dictionary<PictureSlicerHandle, Vector2>();
+
+    PictureSlicerHandle currentHandle;
+
+    void InitHandles() {
+        //
+        handleDic.Add(handle_TopLeft, new Vector2(-1, 1));
+        handleDic.Add(handle_TopRight, new Vector2(1, 1));
+        handleDic.Add(handle_BottomLeft, new Vector2(-1, -1));
+        handleDic.Add(handle_BottomRight, new Vector2(1, -1));
+
+        //把手点击事件
+        foreach (var item in handleDic) {
+            item.Key.onMouseDown = Handle_OnMouseDown;
+            item.Key.onMouseUp = Handle_OnMouseUp;
+        }
+    }
+
+    //更新把手位置
+    void UpdateHandles() {
+        if(currentHandle != null) {
+            Vector2 targetDir = handleDic[currentHandle];
+
+            //不动点位置
+            Vector2 originPos = (Vector2)Camera.main.WorldToScreenPoint(handle_Main.transform.position) + -targetDir * mainHandleSize / 2;
+
+            //动点位置
+            Vector2 desiredPos = (Vector2)Input.mousePosition + mouseOffset;
+
+            //限制在范围内
+            desiredPos = GetClampedHandlePos(desiredPos);
+
+            //修正为方形
+            //获取最短边
+            Vector2 desiredPosOffset = desiredPos - originPos;
+            float shortest = Mathf.Min(Mathf.Abs(desiredPosOffset.x), Mathf.Abs(desiredPosOffset.y));
+            desiredPos = originPos + targetDir * shortest;
+
+            //修改截取器尺寸位置
+            Set(originPos, desiredPos);
+        }
+    }
+
+    //将目标位置限制在可动范围内
+    Vector2 GetClampedHandlePos(Vector2 uiPos) {
+        //获取可动范围内
+        Vector2 center = Camera.main.WorldToScreenPoint(picture.transform.position);
+        Vector2 movementAreaTopRight = center + size / 2;
+        Vector2 movementAreaBottomLeft = center - size / 2;
+
+        //将目标位置限制在可动范围内
+        uiPos.x = Mathf.Clamp(uiPos.x, movementAreaBottomLeft.x, movementAreaTopRight.x);
+        uiPos.y = Mathf.Clamp(uiPos.y, movementAreaBottomLeft.y, movementAreaTopRight.y);
+
+        return uiPos;
+    }
+
+    //鼠标按下和松开把手
+    void Handle_OnMouseDown(PictureSlicerHandle handle) {
+        currentHandle = handle;
+
+        mouseOffset = Camera.main.WorldToScreenPoint(handle.transform.position) - Input.mousePosition;
+    }
+    void Handle_OnMouseUp(PictureSlicerHandle handle) {
+        currentHandle = null;
+    }
+
+    #endregion
+
+    #region 截取器尺寸
+
+    //截取器尺寸
+    Vector2 mainHandleSize {
+        get {
+            return handle_Main.GetComponent<RectTransform>().sizeDelta;
+        }
+    }
+
+    //设置截取器尺寸
+    void SetSize(Vector2 size) {
+        handle_Main.GetComponent<RectTransform>().sizeDelta = size;
+    }
+
+    #endregion
+
+    #region 其他
+
+    //将图片裁剪器填满整张图
+    void Fill() {
+        //设置尺寸为图片尺寸
+        SetSize(size);
+
+        //移动到中心
+        TryMove(transform.position);
+    }
+
+    //通过两个点设置截取器位置和尺寸
+    void Set(Vector2 pos1, Vector2 pos2) {
+        //尺寸
+        Vector2 size = new Vector2(Mathf.Abs(pos2.x - pos1.x), Mathf.Abs(pos2.y - pos1.y));
+        SetSize(size);
+
+        //中心
+        Vector2 center = pos1 + (pos2 - pos1) / 2;
+        TryMove(center);
+    }
+
+    #endregion
 }
---
+++ Assets/Scripts/PictureSlicerHandle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class PictureSlicerHandle : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+    public Action<PictureSlicerHandle> onMouseDown, onMouseUp;
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData) {
+        onMouseDown?.Invoke(this);
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData) {
+        onMouseUp?.Invoke(this);
+    }
+}
