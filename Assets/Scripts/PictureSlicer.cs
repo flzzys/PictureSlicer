13b3a065f6ab6a95a3da6fe93663763d3f5cd523
Zyf <flzzyf@sina.com>
2021-01-27 09:08:50 +0800

    Init

---
+++ Assets/Scripts/PictureSlicer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureSlicer : MonoBehaviour {
+    public SizeManipulator sizeManipulator;
+
+    Vector2 size;
+
+    private void Awake() {
+        size = GetComponent<RectTransform>().sizeDelta;
+
+
+    }
+}
